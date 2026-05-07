5969451fdc2847ca6ab65dd5e9aafafdf6c3d386
Deneth Nadeeja <denethsaputhanthri@gmail.com>
2026-05-07 23:56:54 +0530

    configure session management to use stateless sessions

--- src/main/java/edu/icet/config/SecurityConfig.java
+++ src/main/java/edu/icet/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -25,6 +26,8 @@ public class SecurityConfig {
                         .requestMatchers("/public/**").permitAll()
                 .anyRequest().authenticated())
                 .httpBasic(Customizer.withDefaults())
+                .sessionManagement(session-> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .build();
     }
     @Bean
