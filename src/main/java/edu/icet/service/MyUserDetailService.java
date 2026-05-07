15faf7b16de101be9892295bbd2001a479c5630a
Deneth Nadeeja <denethsaputhanthri@gmail.com>
2026-05-08 02:58:27 +0530

    add MyUserDetailService for user authentication and update SecurityConfig for password encoding

---
+++ src/main/java/edu/icet/service/MyUserDetailService.java
@@ -0,0 +1,35 @@
+package edu.icet.service;
+
+import edu.icet.model.User;
+import edu.icet.model.UserPrinciple;
+import edu.icet.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyUserDetailService implements UserDetailsService {
+    @Autowired
+    private UserRepository repository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        User byUsername = repository.findByUsername(username);
+        if (byUsername == null) {
+            throw new UsernameNotFoundException("User not found with username: " + username);
+        }
+        return new UserPrinciple(byUsername);
+    }
+
+    public void saveUser(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        repository.save(user);
+    }
+}
